8acd0465341960c6f593982527f287e3e19ba211
AinoJoel <ainojoe77@gmail.com>
2023-11-07 09:26:01 +0300

    added middleware to show path and method of each request sent to the server

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,12 @@ const mongoose = require('mongoose')
 //express app
 const app = express()
 
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next()
+})
+
 //routes
 
 //connect to db
