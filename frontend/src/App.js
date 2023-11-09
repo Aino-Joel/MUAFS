5284bd9cd3427fe87a2a3fa09a4e9476c92cb4bf
jauhara-nk <jauharakitty@gmail.com>
2023-11-09 16:38:05 +0300

    dean-page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './header'
+import Sidebar from './sidebar'
+import Home from './home'
+
+
+ 
+   
 
 function App() {
+  // const [openSidebar, setOpenSidebar] = useState(false);
+
+  // const OpenSidebar = () => {
+  //   setOpenSidebar(!openSidebar)
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid-container'>
+      <Header />
+      <Sidebar />
+      <Home />
+
+
     </div>
   );
 }
 
-export default App;
+export default App
+  
+
+
+
