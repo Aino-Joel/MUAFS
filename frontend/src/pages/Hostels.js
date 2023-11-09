c1f00e1f7ebbcc76776179ba15a45029723df85c
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-09 10:44:52 +0300

    the pages

--- frontend/src/pages/Hostels.js
+++ frontend/src/pages/Hostels.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 function Hostels() {
   return (
     <div>
+        <Header/>
       hostels
     </div>
   )
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './Header'
 
 function Signup() {
   return (
     <div>
+        <Header/>
       signup
     </div>
   )
