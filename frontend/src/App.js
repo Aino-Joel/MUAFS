cce10cd23118d5767686d2c00ad5bc93c468ad92
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-09 10:44:16 +0300

    adding routes to the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Hostels from './pages/Hostels';
 
 function App() {
   return (
     <div className="App">
-     frontend
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/hostels' element={< Hostels />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/login' element={< Login />}/>
+        
+      </Routes>
+      </BrowserRouter>
+  
     </div>
   );
 }
