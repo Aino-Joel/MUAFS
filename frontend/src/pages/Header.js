7e1e10d0abdf28a08bb8699ea7f55abb10fd576f
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-09 10:44:31 +0300

    modifications

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="Header">
-        <Link to={'/'}>Home</Link>
+        <br/>
+      <img src = "https://sph.mak.ac.ug/sites/default/files/inline-images/WEB%20Mak-3.png" class = "product-thumb" alt = ""/>
+                    
+      <Link to={'/'}>Home</Link>
       <Link to={'/hostels'}>Hostels</Link>
       <Link to={'/login'}>Login</Link>
       <Link to={'/signup'}>Sign Up</Link>
+      
      
     </div>
   )
