29019928b5c823f6545b01843efe92e06d71f960
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-09 15:33:42 +0300

    adding the hero section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Header from './Header'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <Header/>
-      HOME
+      <section className="hero"  >
+      <div className="content">
+        <h1>MUAFS</h1>
+        <p>
+          {" "}
+          Trust us to get you a home away from home
+        </p>
+        <Link to={'/hostels'}>Start Now</Link>
+      
+      </div>
+    </section>
     </div>
   )
 }
