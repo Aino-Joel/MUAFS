dfec88970f9228e458d56ed7ff898b18584f9298
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-10 10:17:54 +0300

    adding css and modifications

--- frontend/src/pages/Header.js
+++ frontend/src/pages/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="Header">
         <br/>
-      <img src = "https://sph.mak.ac.ug/sites/default/files/inline-images/WEB%20Mak-3.png" class = "product-thumb" alt = ""/>
+      <img src = "https://sph.mak.ac.ug/sites/default/files/inline-images/WEB%20Mak-3.png" class = "img" alt = ""/>
                     
       <Link to={'/'}>Home</Link>
       <Link to={'/hostels'}>Hostels</Link>
