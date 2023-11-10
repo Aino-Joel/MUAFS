28446d44475d40acc76e0e4aa07045aa35dabd1c
AinoJoel <ainojoe77@gmail.com>
2023-11-10 09:53:56 +0300

    Added error handling incase a hostel doesn't exist

--- backend/routes/HostelRoutes.js
+++ backend/routes/HostelRoutes.js
@@ -17,6 +17,10 @@ router.get('/:id', (req, res) => {
 
     const hostel = Hostel.findById(id)
 
+    if(!hostel){
+        return res.status(404).json({error: error.message})
+    }
+
     res.status(200).json(hostel)
 })
 
