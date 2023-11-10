b1e66a39191905649c47e64057d18e0c958f9dcc
AinoJoel <ainojoe77@gmail.com>
2023-11-10 09:43:01 +0300

    Create route to retrieve all hostels from db

--- backend/routes/HostelRoutes.js
+++ backend/routes/HostelRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const Hostel = require('../models/HostelModel.js')
+const mongoose = require('mongoose')
 
 const router = express.Router()
 
 //GET all hostels
+router.get('/', (req, res) => {
+    const hostels = Hostel.find()
 
+    res.status(200).json(hostels)
+})
 
 //GET single hostel
 
