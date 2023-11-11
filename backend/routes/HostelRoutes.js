99c60ce098e7901e8e5df88af20538da99b556cd
AinoJoel <ainojoe77@gmail.com>
2023-11-11 19:24:30 +0300

    Populated hostel controller

--- backend/routes/HostelRoutes.js
+++ backend/routes/HostelRoutes.js
@@ -1,31 +1,11 @@
 const express = require('express')
-const Hostel = require('../models/HostelModel.js')
-const mongoose = require('mongoose')
 
 const router = express.Router()
 
 //GET all hostels
-router.get('/', async (req, res) => {
-    const hostels = await Hostel.find()
-
-    res.status(200).json(hostels)
-})
+router.get('/', )
 
 //GET single hostel
-router.get('/:id', async (req, res) => {
-    const {id} = req.params
-
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: "No such Hostel"})
-    }
-
-    const hostel = await Hostel.findById(id)
-
-    if(!hostel){
-        return res.status(404).json({error: error.message})
-    }
-
-    res.status(200).json(hostel)
-})
+router.get('/:id', )
 
 module.exports = router
