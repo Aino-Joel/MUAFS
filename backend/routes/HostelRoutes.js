0ddcf6dc84608e1846aa0d29d6d2825f5c3a2639
AinoJoel <ainojoe77@gmail.com>
2023-11-11 19:31:33 +0300

    Imported contoller in routes

--- backend/routes/HostelRoutes.js
+++ backend/routes/HostelRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const {
+    getHostels,
+    getHostel
+} = require('../controllers/hostelController')
 
 const router = express.Router()
 
 //GET all hostels
-router.get('/', )
+router.get('/', getHostels)
 
 //GET single hostel
-router.get('/:id', )
+router.get('/:id', getHostel)
 
 module.exports = router
