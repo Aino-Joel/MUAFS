4dec83144923083d78eafaa9b20a400e91fbf418
AinoJoel <ainojoe77@gmail.com>
2023-11-15 02:43:50 +0300

    Added jwt for authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const hostelRoutes = require('./routes/hostels')
+const userRoutes = require('./routes/user')
 
 //express app
 const app = express()
@@ -16,7 +17,8 @@ app.use((req, res, next) => {
 })
 
 //routes
-app.use('/hostels', hostelRoutes)
+app.use('/api/hostels', hostelRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
