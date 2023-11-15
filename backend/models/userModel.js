09ba2e34ee5b2fe57e334376a127b86177a28e6a
AinoJoel <ainojoe77@gmail.com>
2023-11-15 10:31:35 +0300

    Created static login method

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -51,4 +51,25 @@ userSchema.statics.signup = async function(email, regNo, password) {
     return user
 }
 
+//static login method
+userSchema.statics.login = async function(email, regNo, password){
+    if(!email || !regNo || !password){
+        throw Error('All fields should be filled')
+    }
+
+    const user = await this.findOne({email})
+
+    if (!user){
+        throw Error('Incorrect email')
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error('Incorrect password')
+    }
+
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
