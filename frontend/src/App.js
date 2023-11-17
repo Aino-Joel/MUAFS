b3f707aef64a9db614b6744d4342819101297962
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-17 12:58:14 +0300

    deletions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Hostels from './pages/Hostels';
-import Details from './pages/Details';
 
 function App() {
   return (
     <div className="App">
-      
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/hostels' element={< Hostels />}/>
-        <Route path='/signup' element={<Signup />}/>
-        <Route path='/login' element={< Login />}/>
-      </Routes>
-      </BrowserRouter>
-  
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
