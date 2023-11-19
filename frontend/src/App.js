40d28b92c7f9e48b55145b30e5999086ffa12db0
jauhara-nk <jauharakitty@gmail.com>
2023-11-19 22:13:15 +0300

    react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,71 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useState} from 'react';
 // import { useState } from 'react'
 import './App.css';
 // import Header from './header'
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/sidebar'
 import Home from './components/home'
 import Header from './components/header';
-import FormComponent from './components/register';
+import Registration from './components/createaccount';
+import ViewHostels from './hostels';
+// import Register from './components/createaccount';
+//  import ViewHostels from './hostels';
 
 
- 
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+  
+// } from "react-router-dom"
+
    
 
 function App() {
-  // const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
-  // const OpenSidebar = () => {
-  //   setOpenSidebarToggle(!openSidebarToggle)
-  //  }
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+   }
 
   return (
     <div className='grid-container'>
-      {/* <Header OpenSidebar={OpenSidebar}/>
-      <Sidebar  openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-      <Home /> */}
-      <Header />
-      <Sidebar />
-      <Home />
+<Home />
+<Header />
+<Sidebar />
+
       <Routes>
-        <Route path="/register.js" element= {FormComponent} />
+        <Route
+        path='/createaccount'
+        element = {<Registration />}
+        ></Route>
+        <Route 
+        path='/hostels'
+        element = {<ViewHostels />}
+        
+        
+        
+        >
+
+        </Route>
       </Routes>
 
 
+
+
+
+
+
+      {/* <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar  openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home /> */}
+      {/* <Header />
+      <Sidebar />
+      <Home /> */}
+
+
+      
     </div>
   );
 }
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,68 +1,80 @@
 import React from 'react'
+import { Outlet, Link } from 'react-router-dom'
 
 import 
 {BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
   BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
  from 'react-icons/bs'
 
-function Sidebar({openSidebarToggle, OpenSidebar}) {
+ function Sidebar({openSidebarToggle, OpenSidebar}) {
 
 
     
 
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-            <svg xmlns="http://www.w3.org/2000/svg" height="50" width="-300" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
-</svg> CampusCrib
-            </div>
+         <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+         <div className='sidebar-title'>
+             <div className='sidebar-brand'>
+             <svg xmlns="http://www.w3.org/2000/svg" height="50" width="-300" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+   <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
+ </svg> CampusCrib
+             </div>
 
              <span className='icon close_icon'  onClick={OpenSidebar}>X</span> 
 
         </div>
-<hr/>
-        <ul className='sidebar-list'>
+ <hr/>
+         <ul className='sidebar-list'>
             {/* <li className='sidebar-list-item'>
-                <a href="">
-                    <BsGrid1X2Fill className='icon'/> 
+                 <a href="">
+                     <BsGrid1X2Fill className='icon'/> 
                 </a>
-            </li> */}
+             </li> */}
+             <li className='sidebar-list-item'>
+                 <Link to = '/createaccount'>
+                     <BsFillArchiveFill className='icon'/> Create Hostel Account
+                  </Link>
+              </li>
+             {/* <li className='sidebar-list-item'>
+                 <a href="">
+//                     <BsFillGrid3X3GapFill className='icon'/> Categories
+//                 </a>
+//             </li> */}
+           {/* <li className='sidebar-list-item'>
+//                 <a href="">
+//                     <BsPeopleFill className='icon'/> Students
+//                 </a>
+//             </li> */}
             <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillArchiveFill className='icon'/> Create Hostel Account
-                </a>
-            </li>
-            {/* <li className='sidebar-list-item'>
-                <a href="">
-                    <BsFillGrid3X3GapFill className='icon'/> Categories
-                </a>
-            </li> */}
-            {/* <li className='sidebar-list-item'>
-                <a href="">
-                    <BsPeopleFill className='icon'/> Students
-                </a>
-            </li> */}
-            <li className='sidebar-list-item'>
-                <a href="">
+                <Link to = '/hostels'>
                     <BsListCheck className='icon'/> View Hostels
-                </a>
+                </Link>
             </li>
-            <li className='sidebar-list-item'>
-                <a href="">
+             <li className='sidebar-list-item'>
+                 <Link to = '/analytics'>
                     {/* <BsMenuButtonWideFill className='icon'/> Reports/ Issues */}
                     <BsMenuButtonWideFill className='icon'/> Analytics
-                </a>
+                </Link>
             </li>
             <li className='sidebar-list-item'>
-                <a href="">
+                <Link to = '/logout'>
                     <BsFillGearFill className='icon'/> Log out
-                </a>
+                </Link>
             </li>
         </ul>
     </aside>
   )
-}
+ }
+ 
 
+
+
+// const Sidebar = [
+//     {
+//         title: 'Create Hostel Account',
+//         path: "/",
+//         icon: <BsFillArchiveFill />,
+//         cName: 'icon'
+//     }
+// ]
 export default Sidebar
