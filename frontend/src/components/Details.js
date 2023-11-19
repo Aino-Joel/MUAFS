96647e0645b85267f489f19d4bfddd3725f143f2
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-19 07:28:39 +0300

    the hostel details page

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from './Header'
+import { Link } from "react-router-dom";
 
 function Details() {
   return (
     <div>
-        <Header/>
-      details
+      <Header/>
+      <section className="hero"  >
+      <div className="content">
+      <Link to={'/art'}>Book a Room</Link>
+      </div>
+    </section>
     </div>
   )
 }
