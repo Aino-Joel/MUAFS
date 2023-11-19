b0f1ebcb218c7db567ac8a67676f8d88f9576d28
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-19 07:57:52 +0300

    adding the description

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -74,6 +74,24 @@ function Details() {
 
     
     </div>
+    <div class = "description">
+                <h2 class = "description-category">Description</h2>
+                <div class = "description-container">
+                    
+                
+              loren ipsum This website uses cookies and similar technologies to understand visitor experiences. By using this website, you consent to UNC-Chapel Hill's cookie usage in accordance with their Privacy Notice.
+I Accept
+skip to the end of the global utility bar
+THE UNIVERSITY of NORTH CAROLINA at CHAPEL HILL
+AccessibilityEventsLibrariesMapsDepartmentsConnectCarolinaUNC Search
+Sk
+Original paragraph
+
+
+            </div>
+            <br/>
+            <h4 class = "heading">Rooms available</h4>
+            </div>
     </div>
   )
 }
