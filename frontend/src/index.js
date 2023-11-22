28ccd4e7e567b61df670eef43f99569df36c5070
AinoJoel <ainojoe77@gmail.com>
2023-11-22 18:29:42 +0300

    Creating hostel context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { HostelsContextProvider } from './context/HostelContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HostelsContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </HostelsContextProvider>
   </React.StrictMode>
 );
