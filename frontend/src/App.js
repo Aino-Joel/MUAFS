9531d1e174e2d9a7c5fd05d10b310b39a64dbe7d
AinoJoel <ainojoe77@gmail.com>
2023-11-22 13:52:09 +0300

    prettified and rearraged code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
-import Homepage from './pages/Homepage';
-import Login from './pages/login';
-import { Route, Routes } from 'react-router-dom';
+import React, { useState } from "react";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/login";
+import { Route, Routes } from "react-router-dom";
 // import RoomManagement from './RoomManagement/RoomManagement';
-import CreateAccount from './pages/CreateAccount';
-import CreateAccount2 from './pages/CreateAccount2';
+import CreateAccount from "./pages/CreateAccount";
+import CreateAccount2 from "./pages/CreateAccount2";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import Entry from './components/Entry';
 // import Services from './components/Services';
 // import Ratings from './components/Ratings';
-import Home from './components/Home'
-import Details from './components/Details';
-import Book from './components/Book';
-import Hostels from './components/Hostels';
-import Header from './components/Header'
-import Footer from './components/Footer';
+import Home from "./components/Home";
+import Details from "./components/Details";
+import Book from "./components/Book";
+import Hostels from "./components/Hostels";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className='App'>
-
-      <Header/>
+    <div className="App">
+      <Header />
 
       <Routes>
-        <Route path="/admin" element={ <Homepage/> } />
-        <Route path="/Login" element={<Login/> } />
+        <Route path="/admin" element={<Homepage />} />
+        <Route path="/Login" element={<Login />} />
         {/* <Route path="/NotificationsPage" 
               render={() => <NotificationsPage Notifications={notifications} />}
             />
@@ -34,14 +33,13 @@ function App() {
         <Route path="/CreateAccount" element={<CreateAccount />} />
         <Route path="/CreateAccount2" element={<CreateAccount2 />} />
 
-        <Route path='/' element={<Home />}/>
-        <Route path='/hostels' element={< Hostels />}/>
-        <Route path='/details' element={< Details/>}/>
-        <Route path='/book' element={< Book/>}/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/hostels" element={<Hostels />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/book" element={<Book />} />
       </Routes>
 
-      <Footer/>
-      
+      <Footer />
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,17 +2,18 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    
-    <div className='Header'>
-      <br/>
-      <img src = "https://sph.mak.ac.ug/sites/default/files/inline-images/WEB%20Mak-3.png" class = "img" alt = ""/>
-                
-        
-      <Link to={'/'}>Home</Link>
-      <Link to={'/hostels'}>Hostels</Link>
-      <Link to={'/login'}>Login</Link>
-      <Link to={'/signup'}>Sign Up</Link>
-     
+    <div className="Header">
+      <br />
+      <img
+        src="https://sph.mak.ac.ug/sites/default/files/inline-images/WEB%20Mak-3.png"
+        class="img"
+        alt=""
+      />
+
+      <Link to={"/"}>Home</Link>
+      <Link to={"/hostels"}>Hostels</Link>
+      <Link to={"/login"}>Login</Link>
+      <Link to={"/signup"}>Sign Up</Link>
     </div>
   );
 }
