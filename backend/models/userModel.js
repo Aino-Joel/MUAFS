40459b816a1872bf95ba7d0254a6b203577cd8d2
AinoJoel <ainojoe77@gmail.com>
2023-11-23 21:05:38 +0300

    changes to naming convention

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,96 +1,107 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    fName: {
-        type: String,
-        required: true
-    },
-    lName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    regNo: {
-        type: String,
-        required: false,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  fName: {
+    type: String,
+    required: true,
+  },
+  lName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  regNo: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
 //static signup method
-userSchema.statics.signup = async function(fName, lName, email, regNo, password) {
+userSchema.statics.signup = async function (
+  fName,
+  lName,
+  email,
+  regNo,
+  password
+) {
+  //validation
+  if (!fName || !lName || !email || !password) {
+    throw Error("All fields should be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Passsword is not strong enough");
+  }
 
-   //validation
-    if(!fName || !lName || !email || !password){
-        throw Error('All fields should be filled')
-    }
-    if(!validator.isEmail(email)){
-        throw Error('Email is not valid')
-    }
-    if(!validator.isStrongPassword(password)){
-        throw Error('Passsword is not strong enough')
-    }
+  //check if email or regNo exists
+  const existsEmail = await this.findOne({ email });
+  const existsRegNo = await this.findOne({ regNo });
 
-    //check if email or regNo exists
-    const existsEmail = await this.findOne({email})
-    const existsRegNo = await this.findOne({regNo})
+  if (existsEmail) {
+    throw Error("Email already exists");
+  }
+  if (existsRegNo) {
+    throw Error("User with Registration Number already exists");
+  }
 
-    if (existsEmail){
-        throw Error('Email already exists')
-    }
-    if (existsRegNo){
-        throw Error('User with Registration Number already exists')
-    }
+  //hash password
+  const salt = await bcrypt.genSalt(10);
 
-    //hash password
-    const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt);
 
-    const hash = await bcrypt.hash(password, salt)
+  //create user
+  const user = await this.create({
+    fName,
+    lName,
+    email,
+    regNo,
+    password: hash,
+  });
 
-    //create user
-    const user = await this.create({ fName, lName, email, regNo, password: hash })
-
-    return user
-}
+  return user;
+};
 
 //static login method
-userSchema.statics.login = async function(email, password){
-    if(!email || !password){
-        throw Error('All fields should be filled')
-    }
+userSchema.statics.login = async function (username, password) {
+  if (!username || !password) {
+    throw Error("All fields should be filled");
+  }
 
-    var user = null
+  var user = null;
 
-//allow a user to user either reg no or email
-    if(validator.isEmail(email)){
-        user = await this.findOne({email})
-    } else {
-        user = await this.findOne({regNo: email})
-    }
-    
-    if (!user){
-        throw Error('Incorrect email or Registration Number')
-    }
+  //allow a user to use either reg no or email
+  if (validator.isEmail(username)) {
+    user = await this.findOne({ email: username });
+  } else {
+    user = await this.findOne({ regNo: username });
+  }
 
-    const match = await bcrypt.compare(password, user.password)
+  if (!user) {
+    throw Error("Incorrect Username");
+  }
 
-    if(!match){
-        throw Error('Incorrect password')
-    }
+  const match = await bcrypt.compare(password, user.password);
 
-    return user
-}
+  if (!match) {
+    throw Error("Incorrect password");
+  }
 
-module.exports = mongoose.model('User', userSchema)
+  return user;
+};
+
+module.exports = mongoose.model("User", userSchema);
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await login(email, password);
+    await login(username, password);
   };
 
   return (
@@ -28,6 +28,7 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+
         <button disabled={isLoading}>Login</button>
         {error && <div className="error">{error}</div>}
       </form>
