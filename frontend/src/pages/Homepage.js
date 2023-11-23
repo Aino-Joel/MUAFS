647d1d3fc6a7492ff0bb82d5e077c25e02d218fd
kijjo cosmas <kijjocosmas64@gmail.com>
2023-11-23 19:55:58 +0300

    login update

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import Login from "./Login";
+import Login from "./login";
 import NotificationsPage from "./Notifications";
 //import {useLocation} from 'react-router-dom';
 //import {toast} from 'react-toastify';
