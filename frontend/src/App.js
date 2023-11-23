8d010c8914ede3a44edc3d03bdec0662be3fd1eb
AinoJoel <ainojoe77@gmail.com>
2023-11-23 20:10:13 +0300

    organized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,9 @@ import CustodianDashboard from "./pages/CustodianDashboard";
 import Login from "./pages/login";
 import { Route, Routes } from "react-router-dom";
 // import RoomManagement from './RoomManagement/RoomManagement';
-import CreateAccount from "./pages/CreateAccount";
+// import CreateAccount from "./pages/CreateAccount";
 import CreateAccount2 from "./pages/CreateAccount2";
-import HostelDetails from './pages/hostelDetails'
+import HostelDetails from "./pages/hostelDetails";
 // import logo from './logo.svg';
 import "./App.css";
 // import Entry from './components/Entry';
@@ -14,76 +14,55 @@ import "./App.css";
 import Home from "./components/Home";
 import Details from "./components/Details";
 import Book from "./components/Book";
-import Hostels from "./components/Hostels";
+// import Hostels from "./components/Hostels";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-// import Home2 from "./components/Home2";
 
-import React, {useState} from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/sidebar'
-import Home from './components/home-dean'
-import Header from './components/header-dean';
-import CreateAccount from './components/createaccount';
-import ViewHostels from './components/hostels-dean';
-import './css/header.css';
-import './css/sidebar.css';
-import './css/home.css';
-import './css/account.css';
-import './css/hostels.css'
+// import Sidebar from './components/sidebar'
+import HomeDean from "./components/home-dean";
+// import Header from './components/header-dean';
+import CreateAccount from "./components/createaccount";
+import ViewHostels from "./components/hostels-dean";
+import "./css/header.css";
+import "./css/sidebar.css";
+import "./css/home.css";
+import "./css/account.css";
+import "./css/hostels.css";
 
-
-
-   
 function App() {
-  
   return (
     <div className="App">
-      <Header />
+      <div className="grid-container">
+        {/* <Header /> */}
 
-      <Routes>
-        <Route path="/admin" element={<CustodianDashboard />} />
-        <Route path="/Login" element={<Login />} />
-        {/* <Route path="/NotificationsPage" 
+        <Header />
+        {/* <Sidebar />  */}
+        <Routes>
+          <Route path="/admin" element={<CustodianDashboard />} />
+          <Route path="/Login" element={<Login />} />
+          {/* <Route path="/NotificationsPage" 
               render={() => <NotificationsPage Notifications={notifications} />}
             /> */}
-        <Route path="/HostelDetails" element={<HostelDetails />} />
-        {/* <Route path="/RoomManagement" element={<RoomManagement />}/> */}
-        <Route path="/CreateAccount" element={<CreateAccount />} />
-        <Route path="/CreateAccount2" element={<CreateAccount2 />} />
+          <Route path="/HostelDetails" element={<HostelDetails />} />
+          {/* <Route path="/RoomManagement" element={<RoomManagement />}/> */}
+          <Route path="/CreateAccount" element={<CreateAccount />} />
+          <Route path="/CreateAccount2" element={<CreateAccount2 />} />
 
-        <Route path="/" element={<Home />} />
-        <Route path="/hostels" element={<Hostels />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/book" element={<Book />} />
-      </Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/hostels" element={<Hostels />} /> */}
+          <Route path="/details" element={<Details />} />
+          <Route path="/book" element={<Book />} />
 
-      <Footer />
-    {/* <div className='grid-container'> */}
-
-
-{/* <Header /> */}
-<Sidebar /> 
-
-
-
-      <Routes>
-        <Route path='/' element={<Home />} ></Route>
-        <Route path='/home' element={<Home />} ></Route>
-        <Route path='/hostels' element = {<ViewHostels />}></Route>
-        <Route path='/createaccount'element = {<CreateAccount />} ></Route>
-        <Route path='/hostels' element = {<ViewHostels />}></Route>
-      
-        
-      </Routes>
-  
+          {/* <Route path='/' element={<Home />} ></Route> */}
+          <Route path="/homeDean" element={<HomeDean />}></Route>
+          <Route path="/hostels" element={<ViewHostels />}></Route>
+          <Route path="/createaccount" element={<CreateAccount />}></Route>
+          <Route path="/hostels" element={<ViewHostels />}></Route>
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
 
-export default App
-  
-
-
-
+export default App;
