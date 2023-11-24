f95baa25dfa7a9146f979b07a959d6a69fa86f51
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-11-25 00:05:50 +0300

    changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './Header'
-import Footer from './Footer'
+
 
 function Login() {
   return (
     <div>
         <Header />
         <h1>login</h1>
-        <Footer/>
+       
         
     </div>
   )
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './Header'
-import Footer from './Footer'
+
 
 function Signup() {
   return (
     <div>
         <Header />
         Sign Up
-        <Footer/>
+      
     </div>
   )
 
