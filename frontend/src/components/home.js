d5c711e6288d115ed968db5eb529be1bf5726cdb
AinoJoel <ainojoe77@gmail.com>
2023-12-11 20:01:58 +0300

    updated .env

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('You searched for:', search);
+};
 
   return (
     <div>
@@ -9,6 +17,16 @@ function Home() {
           <h1>CampusCrib</h1>
           <p> Trust us to get you a home away from home</p>
           <Link to={"/hostels"}>Start Now</Link>
+          <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    style={{position: "relative"}}
+                />
+                <button type="submit" style={{position: "absolute"}}><FaSearch /></button>
+            </form>
         </div>
       </section>
     </div>
