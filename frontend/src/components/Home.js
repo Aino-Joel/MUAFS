e4c14312effeacc0a90f1282372925e651c4ff61
Ainomugisha Joel Nagumya <ainojoe77@gmail.com>
2023-12-11 16:26:09 +0000

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,29 +1,14 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import HostelList from "./HostelList";
-import { useHostelsContext } from '../hooks/useHostelsContext'
 
 function Home() {
-  const {hostels, dispatch} = useHostelsContext()
+  const [search, setSearch] = useState('')
 
-  useEffect(() => {
-    const fetchHostels = async () => {
-      const response = await fetch('/api/hostels')
-      const json = await response.json()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('You searched for:', search);
+};
 
-      if(!response.ok){
-          console.log('Error')
-      }
-
-      if(response.ok){
-          dispatch({type: 'SET_HOSTELS', payload: json})
-      }
-    }  
-
-  fetchHostels()
-    
-  }, [dispatch])
-    
   return (
     <div>
       <section className="hero">
@@ -31,88 +16,17 @@ function Home() {
           <h1>CampusCrib</h1>
           <p> Trust us to get you a home away from home</p>
           <Link to={"/hostels"}>Start Now</Link>
+          <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
         </div>
       </section>
-      <div className="product">
-        <h2 class="product-category">Recommended for you</h2>
-        <div class="product-container">
-          {hostels && hostels.map((hostel) => (
-            <HostelList key={hostel._id} hostel={hostel} />
-          ))}
-        </div>
-      </div>
-      
-      <div className="product">
-        <h2 className="product-category">
-          Popular Residences <tr />
-        </h2>
-        <span className="price">
-          <Link to={"/hostels"}>View all hostels</Link>
-        </span>
-        <br />
-        <div className="product-container">
-          <div className="product-card">
-            <div className="product-image">
-              <img
-                src="./pictures/room1.jpg"
-                className="product-thumb"
-                alt=""
-              />
-            </div>
-            <div className="product-info">
-              <h2 className="product-brand">Olympia Hostel, Kikoni</h2>
-            </div>
-          </div>
-          <div className="product-card">
-            <div className="product-image">
-              <img
-                src="./pictures/room1.jpg"
-                className="product-thumb"
-                alt=""
-              />
-            </div>
-            <div className="product-info">
-              <h2 className="product-brand">Julianna Girls' Hostel, Kikoni</h2>
-            </div>
-          </div>
-          <div className="product-card">
-            <div className="product-image">
-              <img
-                src="./pictures/room1.jpg"
-                className="product-thumb"
-                alt=""
-              />
-            </div>
-            <div className="product-info">
-              <h2 className="product-brand">Aryan Hostel, Wandegeya</h2>
-            </div>
-          </div>
-          <div className="product-card">
-            <div className="product-image">
-              <img
-                src="./pictures/room1.jpg"
-                className="product-thumb"
-                alt=""
-              />
-            </div>
-            <div className="product-info">
-              <h2 className="product-brand">Muhika Hostel, Kikoni</h2>
-            </div>
-          </div>
-          <div className="product-card">
-            <div className="product-image">
-              <img
-                src="./pictures/room1.jpg"
-                className="product-thumb"
-                alt=""
-              />
-            </div>
-            <div className="product-info">
-              <h2 className="product-brand">Baskon Hostel, Kikoni</h2>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
