5217c1a001aaf6c299852e0f9cff33368d9ac8b8
Ainomugisha Joel Nagumya <ainojoe77@gmail.com>
2023-12-11 19:05:13 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,49 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const hostelRoutes = require('./routes/hostels')
-const userRoutes = require('./routes/user')
-const bookingRoutes = require('./routes/bookingRoutes')
-const custodianRoutes = require('./routes/custodian')
-const cors = require('cors');  
+const express = require("express");
+const mongoose = require("mongoose");
+const hostelRoutes = require("./routes/hostels");
+const userRoutes = require("./routes/user");
+const bookingRoutes = require("./routes/bookingRoutes");
+const custodianRoutes = require("./routes/custodian");
+const cors = require("cors");
 
 const corsOpts = {
-    origin: 'https://muafs-frontend.vercel.app',
-    credentials: true,
-    methods: ['GET','POST','HEAD','PUT','PATCH','DELETE'],
-    allowedHeaders: ['Content-Type'],
-    exposedHeaders: ['Content-Type']
+  origin: "https://muafs-frontend.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  exposedHeaders: ["Content-Type"],
 };
 
 //express app
-const app = express()
+const app = express();
 
 //middleware
 app.use(cors(corsOpts));
 
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 //routes
-app.use('/api/hostels', hostelRoutes)
-app.use('/api/user', userRoutes)
-app.use('/:_id/booking', bookingRoutes);
-app.use('/api/custodian', custodianRoutes);
+app.use("/api/hostels", hostelRoutes);
+app.use("/api/user", userRoutes);
+app.use("/:_id/booking", bookingRoutes);
+app.use("/api/custodian", custodianRoutes);
 
 //connect to db
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        //listen for requests
-        app.listen(process.env.PORT, () => {
-        console.log('connected to db & listening on port ',process.env.PORT)
-    })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
